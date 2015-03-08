8d4d82babd36e3df26a3fe0ab8604416cbb7f2fb
apillalamarri <anupama.pillalamarri@gmail.com>
2015-03-08 15:05:49 -0400

    Update khiyali-word-game.py
    More function-y  and has an outer loop so you can play multiple rounds, but now need to correct some weird behavior including:
    1. It doesn't tell you you're wrong when you make a bad guess.
    2. It doesn't tell you when you've already guessed a combination.
    3. It doesn't print the current state of the phrase.
    4. It only prints all your guesses after you say whether you want to play again or not (possibly only if you say no; need to test)

--- khiyali-word-game.py
+++ khiyali-word-game.py
@@ -1,65 +1,16 @@
 import csv
 import random
 
-#a list of all letters in the alphabet
-alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-
-#print alphabet
-
-
-#open the CSV file with one phrase per line and then randomly select a phrase and set it to upper case
-with open ('phrases.csv', 'r') as phrasefile:
-	phrase = [line for line in phrasefile]
-	answer_mixed=random.choice(phrase)
-	answer=answer_mixed.upper().strip()
-
-#build a list of all of the letters in the selected phrase, and identify the unique set of letters in the phrase
-all_letters = []
-for letter in answer:
-	if letter in alphabet:
-		all_letters.extend(letter)
-unique_letters = set(all_letters)	
-
-print "answer:"
-print answer
-
-#Build a dictionary, cipher, whose keys are the unique letters in the phrase (actual letters) and whose values (encoded letters) are randomly selected on two criteria: each encoded letter can only be selected once and the encoded letter cannot be the same as the actual letter
-cipher = {}
-avail_letters = alphabet
-for cipher_letter in unique_letters:
-	candidate_letters = [item for item in avail_letters if item not in cipher_letter]
-	cipher[cipher_letter] = random.choice(candidate_letters)
-	avail_letters=[item for item in avail_letters if item not in cipher[cipher_letter]]
-			
-print "cipher:"
-print cipher
-
-"""Build the encoded phrase by replacing the actual letters with the encoded letters, 
-preserving spaces and other punctuation from the original phrase. Sets cur_phrase = the encoded_phrase list.
-cur+phrase will be modified as the user guesses correct parts of the cipher
-"""
-encoded_answer = []
-for character in answer:
-	if character in alphabet:
-		encoded_answer.append(cipher.get(character))
-	else:
-		encoded_answer.append(character)	
-print "encoded_answer:"
-print "".join(encoded_answer)
-cur_phrase = "".join(encoded_answer)
-print "cur_phrase is {0} ".format(cur_phrase)
-
-#Function that handles the user's input
+#Handle user's letter guesses
 def letter_guess(encoded_letter,decoded_letter, cur_phrase):
 	if decoded_letter in cipher.keys():
-			
 		if cipher[decoded_letter]==encoded_letter:
 			cur_phrase = "".join(cur_phrase).replace(encoded_letter,decoded_letter.lower())
 			return cur_phrase
 	else:
 		return "WRONG!"
-
-
+		
+#Add user's guesses to the guess dictionary
 def add_guess (encoded_letter,decoded_letter,guess_dict):
 	if encoded_letter in guess_dict.keys():
 		guess_dict[encoded_letter].append(decoded_letter)
@@ -67,62 +18,154 @@ def add_guess (encoded_letter,decoded_letter,guess_dict):
 		guess_dict[encoded_letter] = [decoded_letter]
 
 
-#Loop that handles the possible choices a user can make, and sends inputs to respective functions		
-guess_dict={}
+def choose_phrase(phrase_file):
+	"Randomly selects a phrase from the phrase_file and sets it to uppercase"
+	phrase = [line for line in phrase_file]
+	answer_mixed=random.choice(phrase)
+	answer=answer_mixed.upper().strip()
+	print "answer:"
+	print answer
+	return answer
 
-while True:
+def build_cipher(answer, alphabet):
+	"builds a set of the unique letters in answer, then builds a cipher for that answer"
+	all_letters = []
+	for letter in answer:
+		if letter in alphabet:
+			all_letters.extend(letter)
+	unique_letters = set(all_letters)	
 	
-	choice = raw_input('Would you like to "G"uess a letter or "S"olve the puzzle or "Q"uit? ')
-	if choice.upper() == 'G':
-		keep_guessing = 1
-		while keep_guessing:
-			encoded_letter = raw_input('Enter the letter you wish to decode: ').upper()
-			if (encoded_letter in encoded_answer):
-				keep_guessing = 0
-				decoded_letter = raw_input('Enter the letter you think is represented by {0}: '.format(encoded_letter)).upper()
-				#print letter_guess(encoded_letter,decoded_letter,cur_phrase)
-				if decoded_letter in cipher.keys():		
-					if cipher[decoded_letter]==encoded_letter:
-						cur_phrase = "".join(cur_phrase).replace(encoded_letter,decoded_letter.lower())
-						if cur_phrase.upper()==answer:
-							print "Your answer, so hot right now!"
-							print cur_phrase.upper()+" is correct!"
-							break
-						else:
-							print "cur_phrase is " + cur_phrase
-				else:
-					print "WRONG!"
-					print "cur_phrase is " + cur_phrase
+	#Build a dictionary, cipher, whose keys are the unique letters in the phrase (actual letters) and whose values (encoded letters) are randomly selected on two criteria: each encoded letter can only be selected once and the encoded letter cannot be the same as the actual letter
+	cipher = {}
+	avail_letters = alphabet
+	for cipher_letter in unique_letters:
+		candidate_letters = [item for item in avail_letters if item not in cipher_letter]
+		cipher[cipher_letter] = random.choice(candidate_letters)
+		avail_letters=[item for item in avail_letters if item not in cipher[cipher_letter]]
 				
-				
-				
-				add_guess(encoded_letter,decoded_letter,guess_dict)
-				#print current_phrase(cur_phrase,encoded_letter,decoded_letter)
-				print "guess_dict:"
-				print guess_dict
-				
-			elif len(encoded_letter) > 1:
-				print "You may only enter one letter at a time. Please try again."
-			else:
-				print encoded_letter + " is not part of the encoded phrase. Please try again."
-			
-				
+	print "cipher:"
+	print cipher
+	return cipher
 	
-	#Handle letter guess
-	elif choice.upper() =='S':
+def build_encoded_answer(answer, cipher, alphabet):
+	"""Build the encoded answer by replacing the actual letters with the encoded letters, preserving spaces and other punctuation from the original phrase. Sets cur_phrase = the encoded_phrase list. cur_phrase will be modified as the user guesses correct parts of the cipher"""
 	
-	#Handle solve
-		solution = raw_input('Please enter your solution: ')
-		print "Solution:"
-		print solution.upper()
+	encoded_answer = []
+	for character in answer:
+		if character in alphabet:
+			encoded_answer.append(cipher.get(character))
+		else:
+			encoded_answer.append(character)	
+	print "encoded_answer:"
+	print "".join(encoded_answer)
+	return "".join(encoded_answer)
+	
+#cur_phrase = "".join(encoded_answer)
+#print "cur_phrase is {0} ".format(cur_phrase)
+
+
+def handle_valid_guess(encoded_letter, decoded_letter, encoded_answer, cipher, guess_dict, cur_phrase):
+	"Add the guess to guess_dict. If it's correct, update cur_phrase. If not, tell the user it's wrong." 
+	add_guess(encoded_letter,decoded_letter,guess_dict)
+	#print letter_guess(encoded_letter,decoded_letter,cur_phrase)
+	if decoded_letter in cipher.keys():
+		if cipher[decoded_letter]==encoded_letter:
+			print "Yes, " + encoded_letter + " does represent " + decoded_letter + "!"
+			cur_phrase = "".join(cur_phrase).replace(encoded_letter,decoded_letter.lower())
+		else:
+			print "WRONG!"
+			print "cur_phrase is " + cur_phrase		
+	return cur_phrase		
+					
 		
-		if solution.upper() == answer:
-			print "Your answer, so hot right now!"
-			print solution.upper() + " is correct!"
-			break
-	elif choice.upper() =='Q':
-		print "Kthxbai!"
-		break
-		
+def win_play_again(solution):
+	"Tell the user that s/he's won and see if s/he wants to play again."
+	print "Your answer, so hot right now!"
+	print solution.upper() + " is correct!"
+	play_again = raw_input('Play again?(Y/N): ').upper()
+	while play_again not in ('Y', 'N'):
+		play_again = raw_input('Invalid entry. Play again?(Y/N): ').upper()
+	if play_again == 'Y':
+		return 1
 	else:
-		print "invalid entry; please try again"
+		return 0
+		
+def print_guesses(guess_dict):
+	"Prints all the user's guesses and how many guesses it took to solve the puzzle"
+	count = 0
+	print "Encoded letter\tGuesses"
+	for k, v in guess_dict.iteritems():
+		count = count+1
+		print k+":\t\t:",
+		print v
+	print "You solved this in {0} guesses!".format(count)
+	
+#a list of all letters in the alphabet
+alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+#Intro message to user:
+print "Khiyali made up this cool cryptography game when she was six.\nShe'd give me a phrase that was encrypted with a cipher she'd generate\non the fly by assigning each letter to a different letter of the alphabet.\nThen I'd have to guess, e.g. 'Does E mean T?' and when I guessed correctly\nshe'd re-write the encrypted phrase with the decrypted letters in lowercase.\nAt any point, I could guess the phrase if I was ready.\n"
+print "Let's play the code game!\n"
+#Loop that handles the possible choices a user can make, and sends inputs to respective functions
+keep_playing = 1
+while keep_playing:
+	#Choose a random phrase and encode it with a cipher
+	with open ('phrases.csv', 'r') as phrase_file:
+		answer = choose_phrase(phrase_file)
+	cipher = build_cipher(answer, alphabet)
+	encoded_answer = build_encoded_answer(answer, cipher, alphabet)
+	cur_phrase = encoded_answer
+	
+	#initialize the guess dictionary guess_dict, which will store the player's guesses during a given round
+	guess_dict={}
+	
+	choice = ''
+	won = 0
+	while ((choice != 'Q') and not won and keep_playing):
+		#Does the user want to guess a letter, solve the puzzle, or quit?
+		choice = raw_input('Would you like to "G"uess a letter or "S"olve the puzzle or "Q"uit? ').upper()
+		if choice == 'G':
+			#User wants to guess a letter
+			keep_guessing = 1
+			while keep_guessing:
+				encoded_letter = raw_input('Enter the letter you wish to decode: ').upper()
+				if len(encoded_letter) > 1:
+					print "You may only enter one letter at a time. Please try again."
+				elif (encoded_letter in encoded_answer):
+					#User has chosen a letter in the code to decode
+					keep_guessing = 0
+					decoded_letter = raw_input('Enter the letter you think is represented by {0}: '.format(encoded_letter)).upper()
+					try:
+						if encoded_letter in guess_dict.get(decoded_letter):
+							#User has already guessed this combination
+							print "You've already guessed that!"
+					except:
+						#User has made a new guess consisting of letter in the code and its possible value
+						cur_phrase = handle_valid_guess(encoded_letter, decoded_letter, encoded_answer, cipher, guess_dict, cur_phrase)
+						if cur_phrase.upper()==answer:
+							#User has guessed the last value needed to solve the puzzle
+							won = 1
+							print_guesses(guess_dict)
+							keep_playing = win_play_again(answer)
+					#print the current state of the guess dictionary
+					#print "guess_dict:"
+					#print guess_dict
+				else:
+					#User has chosen a letter to decode that is not part of the encoded phrase
+					print encoded_letter + " is not part of the encoded phrase. Please try again."
+					print "cur_phrase is " + cur_phrase					
+		elif choice =='S':
+		#Handle solve
+			solution = raw_input('Please enter your solution: ')
+			print "Solution:"
+			print solution.upper()	
+			if solution.upper() == answer:
+				won = 1
+				keep_playing = win_play_again(answer)
+				print_guesses(guess_dict)
+		elif choice =='Q':
+			keep_playing = 0
+			print "Kthxbai!"
+			break
+		else:
+			print "invalid entry; please try again"
